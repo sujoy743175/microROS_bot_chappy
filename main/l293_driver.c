9fa67a4ff494df2825051374df030a3c19b728e1
sujoy <sujoy743175@yahoo.co.in>
2022-01-20 23:47:06 +0530

    initial commit

---
+++ main/l293_driver.c
@@ -0,0 +1,501 @@
+/*
+    IoT Simplified (iotsimplified.com)
+    
+    Sensor Driver for the HC-SR04 Ultrasonic Range Finding Sensor
+
+    Written by Ahmed Al Bayati
+    
+
+
+    Unless required by applicable law or agreed to in writing, this
+    software is distributed on an "AS IS" BASIS, WITHOUT Wfwd_errANTIES OR
+    CONDITIONS OF ANY KIND, either express or implied.
+    
+*/
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_system.h"
+#include "esp_err.h"
+#include "esp_log.h"
+
+#include <driver/ledc.h>
+
+#ifdef ESP_PLATFORM
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#endif
+
+
+/*#define FWD_HCSR_TRIG_GPIO_NUM GPIO_NUM_23
+#define FWD_HCSR_ECHO_GPIO_NUM GPIO_NUM_22
+#define LEFT_HCSR_TRIG_GPIO_NUM GPIO_NUM_14
+#define LEFT_HCSR_ECHO_GPIO_NUM GPIO_NUM_25
+#define RIGHT_HCSR_TRIG_GPIO_NUM GPIO_NUM_33
+#define RIGHT_HCSR_ECHO_GPIO_NUM GPIO_NUM_33*/
+#define LED_BUILTIN 2
+#define HCSR_HIGH 1
+#define HCSR_LOW 0
+
+
+static const char *TAG = "hc-sr04_driver";
+
+// PINS
+//#define LED_BUILTIN 33
+#define PIN_LEFT_FORWARD 26
+#define PIN_LEFT_BACKWARD 27
+#define PIN_RIGHT_FORWARD 12
+#define PIN_RIGHT_BACKWARD 14
+
+// PWM Channels (Reserve channel 0 and 1 for camera)
+#define PWM_LEFT_FORWARD LEDC_CHANNEL_2
+#define PWM_LEFT_BACKWARD LEDC_CHANNEL_3
+#define PWM_RIGHT_FORWARD LEDC_CHANNEL_4
+#define PWM_RIGHT_BACKWARD LEDC_CHANNEL_5
+
+// Other PWM settings
+#define PWM_FREQUENCY 50
+#define PWM_RESOLUTION LEDC_TIMER_12_BIT
+#define PWM_TIMER LEDC_TIMER_1
+#define PWM_MODE LEDC_HIGH_SPEED_MODE
+
+/*// Function Prototypes
+int setup_ledc(); //Setup all the pins and such to get the GPIO ready to be used
+esp_err_t hcsr_send_trig_signal(); //Send a 10uS trig signal HIGH to the sensor
+int hcsr_echo_pulse_read(); //Read the returned pulse and update a variable*/
+
+
+
+// Function Def's
+
+void cmd_vel_callback(const void *msgin);
+
+int hcsr_setup_pins()
+{
+    ESP_LOGI(TAG,"Setting up ledc");
+   /* //Config the Trig Pin
+    gpio_pad_select_gpio(FWD_HCSR_TRIG_GPIO_NUM);
+    gpio_set_direction(FWD_HCSR_TRIG_GPIO_NUM,GPIO_MODE_OUTPUT);
+
+    //Config the Echo Pin
+    gpio_pad_select_gpio(FWD_HCSR_ECHO_GPIO_NUM);
+    gpio_set_direction(FWD_HCSR_ECHO_GPIO_NUM,GPIO_MODE_INPUT);
+
+        //Config the Trig Pin
+    gpio_pad_select_gpio(LEFT_HCSR_TRIG_GPIO_NUM);
+    gpio_set_direction(LEFT_HCSR_TRIG_GPIO_NUM,GPIO_MODE_OUTPUT);
+
+    //Config the Echo Pin
+    gpio_pad_select_gpio(LEFT_HCSR_ECHO_GPIO_NUM);
+    gpio_set_direction(LEFT_HCSR_ECHO_GPIO_NUM,GPIO_MODE_INPUT);
+
+        //Config the Trig Pin
+    gpio_pad_select_gpio(RIGHT_HCSR_TRIG_GPIO_NUM);
+    gpio_set_direction(RIGHT_HCSR_TRIG_GPIO_NUM,GPIO_MODE_OUTPUT);
+
+    //Config the Echo Pin
+    gpio_pad_select_gpio(RIGHT_HCSR_ECHO_GPIO_NUM);
+    gpio_set_direction(RIGHT_HCSR_ECHO_GPIO_NUM,GPIO_MODE_INPUT);
+
+    gpio_reset_pin(LED_BUILTIN);
+    gpio_set_direction(LED_BUILTIN, GPIO_MODE_INPUT_OUTPUT);
+
+    //gpio_set_direction(PIN_LEFT_FORWARD, GPIO_MODE_OUTPUT);
+    //gpio_set_direction(PIN_LEFT_BACKWARD, GPIO_MODE_OUTPUT);
+
+    //ESP_LOGI(TAG,"GPIO Pins Setup Completed");*/
+
+
+    //gpio_reset_pin(LED_BUILTIN);
+    //gpio_set_direction(LED_BUILTIN, GPIO_MODE_INPUT_OUTPUT);
+
+    // Configure 4 PWM channels and assign output pins
+    ledc_channel_config_t ledc_channel[4] = {
+        {
+            .channel    = PWM_LEFT_FORWARD,
+            .duty       = 0,
+            .gpio_num   = PIN_LEFT_FORWARD,
+            .speed_mode = PWM_MODE,
+            .hpoint     = 0,
+            .timer_sel  = LEDC_TIMER_1
+        },
+        {
+            .channel    = PWM_LEFT_BACKWARD,
+            .duty       = 0,
+            .gpio_num   = PIN_LEFT_BACKWARD,
+            .speed_mode = PWM_MODE,
+            .hpoint     = 0,
+            .timer_sel  = LEDC_TIMER_1
+        },
+        {
+            .channel    = PWM_RIGHT_FORWARD,
+            .duty       = 0,
+            .gpio_num   = PIN_RIGHT_FORWARD,
+            .speed_mode = PWM_MODE,
+            .hpoint     = 0,
+            .timer_sel  = LEDC_TIMER_1
+        },
+        {
+            .channel    = PWM_RIGHT_BACKWARD,
+            .duty       = 0,
+            .gpio_num   = PIN_RIGHT_BACKWARD,
+            .speed_mode = PWM_MODE,
+            .hpoint     = 0,
+            .timer_sel  = LEDC_TIMER_1
+        },
+    };
+
+    for (int i = 0; i < 4; i++) {
+        ledc_channel_config(&ledc_channel[i]);
+    }
+    // Configure timer
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = PWM_RESOLUTION,
+        .freq_hz = PWM_FREQUENCY,
+        .speed_mode = PWM_MODE,
+        .timer_num = PWM_TIMER,
+        .clk_cfg = LEDC_AUTO_CLK,
+    };
+    ledc_timer_config(&ledc_timer);
+
+    
+
+    ESP_LOGI(TAG,"ledc Setup Completed NOW!");
+
+    return 0;
+}
+
+/*esp_err_t fwd_hcsr_send_trig_signal()
+{
+    //ESP_LOGI(TAG,"Sending Trig Signal to HCSR04 Sensor");
+    esp_err_t fwd_err;
+    //Signal sent needs to be on HIGH for 10 uS
+    fwd_err = gpio_set_level(FWD_HCSR_TRIG_GPIO_NUM,HCSR_LOW);
+    if (fwd_err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: Trig was unable to be sent");
+        return fwd_err;
+    }
+    vTaskDelay(pdMS_TO_TICKS(0.002));
+
+    fwd_err = gpio_set_level(FWD_HCSR_TRIG_GPIO_NUM,HCSR_HIGH);
+    if (fwd_err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: Trig was unable to be sent");
+        return fwd_err;
+    }
+    vTaskDelay(pdMS_TO_TICKS(0.01));
+
+    fwd_err = gpio_set_level(FWD_HCSR_TRIG_GPIO_NUM,HCSR_LOW);
+    if (fwd_err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: Trig was unable to be sent");
+        return fwd_err;
+    }
+
+    //ESP_LOGI(TAG,"Trig Signal Sending Completed");
+    return fwd_err;
+
+}
+
+esp_err_t left_hcsr_send_trig_signal()
+{
+    //ESP_LOGI(TAG,"Sending Trig Signal to HCSR04 Sensor");
+    esp_err_t left_err;
+    //Signal sent needs to be on HIGH for 10 uS
+    left_err = gpio_set_level(LEFT_HCSR_TRIG_GPIO_NUM,HCSR_LOW);
+    if (left_err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: Trig was unable to be sent");
+        return left_err;
+    }
+    vTaskDelay(pdMS_TO_TICKS(0.002));
+
+    left_err = gpio_set_level(LEFT_HCSR_TRIG_GPIO_NUM,HCSR_HIGH);
+    if (left_err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: Trig was unable to be sent");
+        return left_err;
+    }
+    vTaskDelay(pdMS_TO_TICKS(0.01));
+
+    left_err = gpio_set_level(LEFT_HCSR_TRIG_GPIO_NUM,HCSR_LOW);
+    if (left_err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: Trig was unable to be sent");
+        return left_err;
+    }
+
+    //ESP_LOGI(TAG,"Trig Signal Sending Completed");
+    return left_err;
+
+}
+
+esp_err_t right_hcsr_send_trig_signal()
+{
+    //ESP_LOGI(TAG,"Sending Trig Signal to HCSR04 Sensor");
+    esp_err_t right_err;
+    //Signal sent needs to be on HIGH for 10 uS
+    right_err = gpio_set_level(RIGHT_HCSR_TRIG_GPIO_NUM,HCSR_LOW);
+    if (right_err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: Trig was unable to be sent");
+        return right_err;
+    }
+    vTaskDelay(pdMS_TO_TICKS(0.002));
+
+    right_err = gpio_set_level(RIGHT_HCSR_TRIG_GPIO_NUM,HCSR_HIGH);
+    if (right_err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: Trig was unable to be sent");
+        return right_err;
+    }
+    vTaskDelay(pdMS_TO_TICKS(0.01));
+
+    right_err = gpio_set_level(RIGHT_HCSR_TRIG_GPIO_NUM,HCSR_LOW);
+    if (right_err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: Trig was unable to be sent");
+        return right_err;
+    }
+
+    //ESP_LOGI(TAG,"Trig Signal Sending Completed");
+    return right_err;
+
+}
+
+int fwd_hcsr_echo_pulse_read()
+{
+    // ESP_LOGI(TAG,"Echo Reading");
+    //Pulse Width Input - So we need to read how long is the input held up HIGH and return that value
+    bool no_Signal = true;
+    int fwd_pulse_Width = 0; // in uS
+    int counter = 0;
+    while(no_Signal)
+    {
+        if(gpio_get_level(FWD_HCSR_ECHO_GPIO_NUM) == HCSR_HIGH)
+        {   
+            no_Signal = false;
+            while(gpio_get_level(FWD_HCSR_ECHO_GPIO_NUM) == HCSR_HIGH)
+            {
+                //Calculate the time in Microseconds
+                fwd_pulse_Width += 1;
+                vTaskDelay(pdMS_TO_TICKS(0.001));
+            }
+        }
+        else if(counter > 100)
+        {
+            fwd_hcsr_send_trig_signal();
+            counter = 0;
+        }
+        counter++;
+        
+    }
+    // ESP_LOGI(TAG,"Echo Reading Completed");
+    return fwd_pulse_Width;
+}
+
+int left_hcsr_echo_pulse_read()
+{
+    // ESP_LOGI(TAG,"Echo Reading");
+    //Pulse Width Input - So we need to read how long is the input held up HIGH and return that value
+    bool no_Signal = true;
+    int left_pulse_Width = 0; // in uS
+    int counter = 0;
+    while(no_Signal)
+    {
+        if(gpio_get_level(LEFT_HCSR_ECHO_GPIO_NUM) == HCSR_HIGH)
+        {   
+            no_Signal = false;
+            while(gpio_get_level(LEFT_HCSR_ECHO_GPIO_NUM) == HCSR_HIGH)
+            {
+                //Calculate the time in Microseconds
+                left_pulse_Width += 1;
+                vTaskDelay(pdMS_TO_TICKS(0.001));
+            }
+        }
+        else if(counter > 100)
+        {
+            left_hcsr_send_trig_signal();
+            counter = 0;
+        }
+        counter++;
+        
+    }
+    // ESP_LOGI(TAG,"Echo Reading Completed");
+    return left_pulse_Width;
+}
+
+int right_hcsr_echo_pulse_read()
+{
+    // ESP_LOGI(TAG,"Echo Reading");
+    //Pulse Width Input - So we need to read how long is the input held up HIGH and return that value
+    bool no_Signal = true;
+    int right_pulse_Width = 0; // in uS
+    int counter = 0;
+    while(no_Signal)
+    {
+        if(gpio_get_level(RIGHT_HCSR_ECHO_GPIO_NUM) == HCSR_HIGH)
+        {   
+            no_Signal = false;
+            while(gpio_get_level(RIGHT_HCSR_ECHO_GPIO_NUM) == HCSR_HIGH)
+            {
+                //Calculate the time in Microseconds
+                right_pulse_Width += 1;
+                vTaskDelay(pdMS_TO_TICKS(0.001));
+            }
+        }
+        else if(counter > 100)
+        {
+            right_hcsr_send_trig_signal();
+            counter = 0;
+        }
+        counter++;
+        
+    }
+    // ESP_LOGI(TAG,"Echo Reading Completed");
+    return right_pulse_Width;
+}
+
+
+double fdw_hcsr_get_distance_in()
+{
+    esp_err_t err;
+    int pulse_readout = 0;
+    double fwd_distance = 0.0;
+    
+    err = fwd_hcsr_send_trig_signal();
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: within hcsr_send_trig_signal()");
+        esp_restart();
+    }
+
+    pulse_readout = fwd_hcsr_echo_pulse_read();
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: within hcsr_send_trig_signal()");
+        esp_restart();
+    }
+
+    ESP_LOGI(TAG,"The Value of the Pulse Width is: %d",pulse_readout);
+    fwd_distance = ((double)pulse_readout / 148.0) * 10;
+    //ESP_LOGI(TAG,"Distance in Inches: %f",distance);
+    return fwd_distance;
+}
+
+double left_hcsr_get_distance_in()
+{
+    esp_err_t err;
+    int pulse_readout = 0;
+    double left_distance = 0.0;
+    
+    err = left_hcsr_send_trig_signal();
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: within hcsr_send_trig_signal()");
+        esp_restart();
+    }
+
+    pulse_readout = left_hcsr_echo_pulse_read();
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: within hcsr_send_trig_signal()");
+        esp_restart();
+    }
+
+    ESP_LOGI(TAG,"The Value of the Pulse Width is: %d",pulse_readout);
+    left_distance = ((double)pulse_readout / 148.0) * 10;
+    //ESP_LOGI(TAG,"Distance in Inches: %f",distance);
+    return left_distance;
+}
+
+double right_hcsr_get_distance_in()
+{
+    esp_err_t err;
+    int pulse_readout = 0;
+    double right_distance = 0.0;
+    
+    err = right_hcsr_send_trig_signal();
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: within hcsr_send_trig_signal()");
+        esp_restart();
+    }
+
+    pulse_readout = right_hcsr_echo_pulse_read();
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG,"ERROR: within hcsr_send_trig_signal()");
+        esp_restart();
+    }
+
+    ESP_LOGI(TAG,"The Value of the Pulse Width is: %d",pulse_readout);
+    right_distance = ((double)pulse_readout / 148.0) * 10;
+    //ESP_LOGI(TAG,"Distance in Inches: %f",distance);
+    return right_distance;
+}*/
+
+ 
+/*double hcsr_caliberate_sensor()
+{
+    ESP_LOGI(TAG,"Caliberating Sensor");
+
+    double distance = 0.0;
+
+    vTaskDelay(pdMS_TO_TICKS(10000)); // Wait 10 seconds for sensor stability
+
+    for(int i = 0; i < 10; i++)
+    {
+        // Get 10 readings and average them
+        distance += hcsr_get_distance_in();
+        vTaskDelay(pdMS_TO_TICKS(HCSR_MEASUREMENT_CYCLE_MS));
+    }
+
+    return distance / 10.0;
+}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*          test codes from this poit
+
+
+
+
+
+
+
+
+
+test codes .............................................*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main/main.c
@@ -0,0 +1,311 @@
+/* WiFi station Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdint.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "tcpip_adapter.h"
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+#include "lwip/sockets.h"
+#include "lwip/dns.h"
+#include "lwip/netdb.h"
+
+#include "mqtt_client.h"
+#include "ultrasonic.h"
+#include "ultrasonic.c"
+
+#include <unistd.h>
+#include <uros_network_interfaces.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <std_msgs/msg/int32.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <rmw_microros/rmw_microros.h>
+#include "uxr/client/config.h"
+#include <driver/gpio.h>
+
+#include <inttypes.h>
+#include <math.h>
+#include <geometry_msgs/msg/twist.h>
+#include <driver/ledc.h>
+#include "l293_driver.c"
+
+
+//static const char *TAG = "main_app";
+
+// Macro functions
+#define constrain(amt, low, high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
+
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Aborting.\n",__LINE__,(int)temp_rc);vTaskDelete(NULL);}}
+#define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){printf("Failed status on line %d: %d. Continuing.\n",__LINE__,(int)temp_rc);}}
+
+
+// Constants
+#define FRAME_TIME 100 // 1000 / FRAME_TIME = FPS
+#define SLEEP_TIME 10
+
+// These values are determined by experiment and are unique to every robot
+#define PWM_MOTOR_MIN 1000   // The value where the motor starts moving
+#define PWM_MOTOR_MAX 4095   // Full speed (2^12 - 1)
+
+#ifdef ESP_PLATFORM
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#endif
+
+
+//rcl_publisher_t publisher;
+rcl_publisher_t led_state_publisher;
+rcl_publisher_t fwd_distance_publisher;
+rcl_publisher_t left_distance_publisher;
+rcl_publisher_t right_distance_publisher;
+rcl_subscription_t led_input_subscriber;
+rcl_subscription_t cmd_vel_subscriber;
+
+//rcl_publisher_t publisher;
+//std_msgs__msg__Int32 msg;
+
+
+std_msgs__msg__Int32 LedStateMsg;
+std_msgs__msg__Int32 LedInputMsg;
+std_msgs__msg__Int32 FwdDistanceMsg;
+std_msgs__msg__Int32 LeftDistanceMsg;
+std_msgs__msg__Int32 RightDistanceMsg;
+geometry_msgs__msg__Twist msg;
+
+
+float fmap(float val, float in_min, float in_max, float out_min, float out_max);
+
+
+
+
+void subscription_callback(const void * msgin){
+	const std_msgs__msg__Int32 * msg = (const std_msgs__msg__Int32 *)msgin;	
+	gpio_set_level(LED_BUILTIN, msg->data);
+	
+}
+
+
+void timer_callback(rcl_timer_t *timer, int64_t last_call_time) {
+
+    //gpio_set_level(LED_BUILTIN, !gpio_get_level(LED_BUILTIN));
+
+	if (timer != NULL) {
+
+    	//LedStateMsg.data = gpio_get_level(LED_BUILTIN);
+		//RCSOFTCHECK(rcl_publish(&led_state_publisher, &LedStateMsg, NULL));
+		FwdDistanceMsg.data = 100*distance_FWD();
+		RCSOFTCHECK(rcl_publish(&fwd_distance_publisher, &FwdDistanceMsg, NULL));
+		LeftDistanceMsg.data = 100*distance_LEFT();
+		RCSOFTCHECK(rcl_publish(&left_distance_publisher, &LeftDistanceMsg, NULL));
+		RightDistanceMsg.data = 100*distance_RIGHT();
+		RCSOFTCHECK(rcl_publish(&right_distance_publisher, &RightDistanceMsg, NULL));
+		printf("ultrasound data sent\n");
+		//LedStateMsg.data++;
+	}
+
+    if (timer == NULL) {
+        return;
+    }
+
+	printf("timer passed\n");
+
+    // Use linear.x for forward value and angular.z for rotation
+    float linear = constrain(msg.linear.x, -1, 1);
+    float angular = constrain(msg.angular.z, -1, 1);
+
+    // This robot is an RC tank and uses a differential drive (skid steering).
+    // Calculate the speed of left and right motors. Simple version without wheel distances.
+    // Taken from https://hackernoon.com/unicycle-to-differential-drive-courseras-control-of-mobile-robots-with-ros-and-rosbots-part-2-6d27d15f2010#1e59
+    float left = (linear - angular) / 2.0f;
+    float right = (linear + angular) / 2.0f;
+
+    // Then map those values to PWM intensities. PWM_MOTOR_MAX = full speed, PWM_MOTOR_MIN = the minimal amount of power at which the motors begin moving.
+    uint16_t pwmLeft = (uint16_t) fmap(fabs(left), 0, 1, PWM_MOTOR_MIN, PWM_MOTOR_MAX);
+    uint16_t pwmRight = (uint16_t) fmap(fabs(right), 0, 1, PWM_MOTOR_MIN, PWM_MOTOR_MAX);
+
+    // Each wheel has a channel for forwards and backwards movement
+    ledc_set_duty(PWM_MODE, PWM_LEFT_FORWARD, pwmLeft * (left > 0));
+    ledc_set_duty(PWM_MODE, PWM_LEFT_BACKWARD, pwmLeft * (left < 0));
+    ledc_set_duty(PWM_MODE, PWM_RIGHT_FORWARD, pwmRight * (right > 0));
+    ledc_set_duty(PWM_MODE, PWM_RIGHT_BACKWARD, pwmRight * (right < 0));
+
+    ledc_update_duty(PWM_MODE, PWM_LEFT_FORWARD);
+    ledc_update_duty(PWM_MODE, PWM_LEFT_BACKWARD);
+    ledc_update_duty(PWM_MODE, PWM_RIGHT_FORWARD);
+    ledc_update_duty(PWM_MODE, PWM_RIGHT_BACKWARD);
+
+    printf("%d, %d %d, %d, %d %d, %f, %f\n", pwmLeft, left > 0, left < 0, pwmRight, right > 0, right < 0, left, right);
+}
+
+
+// Helper functions
+// -------------------------------------------------------------------
+
+float fmap(float val, float in_min, float in_max, float out_min, float out_max) {
+    return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+void micro_ros_task(void * arg)
+{
+	rcl_allocator_t allocator = rcl_get_default_allocator();
+	rclc_support_t support;
+
+	rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+	RCCHECK(rcl_init_options_init(&init_options, allocator));
+	rmw_init_options_t* rmw_options = rcl_init_options_get_rmw_init_options(&init_options);
+
+	// Static Agent IP and port can be used instead of autodisvery.
+	RCCHECK(rmw_uros_options_set_udp_address(CONFIG_MICRO_ROS_AGENT_IP, CONFIG_MICRO_ROS_AGENT_PORT, rmw_options));
+	//RCCHECK(rmw_uros_discover_agent(rmw_options));
+
+	// create init_options
+	RCCHECK(rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator));
+
+	// create node
+	rcl_node_t node;
+	RCCHECK(rclc_node_init_default(&node, "blink_on_input", "", &support));
+
+	// create publisher
+	/*RCCHECK(rclc_publisher_init_default(
+		&led_state_publisher,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"led_state"));*/
+
+    // create led_state publisher
+	RCCHECK(rclc_publisher_init_default(
+		&fwd_distance_publisher,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"fwd_distance"));
+
+      // create led_state publisher
+	RCCHECK(rclc_publisher_init_default(
+		&left_distance_publisher,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "left_distance"));
+
+    // create led_state publisher
+	RCCHECK(rclc_publisher_init_default(
+		&right_distance_publisher,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32), "right_distance"));
+	
+		// create subscriber 
+	RCCHECK(rclc_subscription_init_default(
+		&led_input_subscriber,
+		&node,
+		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+		"led_input"));
+	
+	  // create subscriber
+    rcl_subscription_t subscriber;
+    RCCHECK(rclc_subscription_init_default(
+        &subscriber,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist),
+        "/cmd_vel"));
+
+	// create timer,
+	rcl_timer_t timer;
+	const unsigned int timer_timeout = 600;
+	RCCHECK(rclc_timer_init_default(
+		&timer,
+		&support,
+		RCL_MS_TO_NS(timer_timeout),
+		timer_callback));
+
+	// create executor
+	rclc_executor_t executor;
+	RCCHECK(rclc_executor_init(&executor, &support.context, 3, &allocator));
+	RCCHECK(rclc_executor_add_timer(&executor, &timer));
+	RCCHECK(rclc_executor_add_subscription(&executor, &led_input_subscriber,&LedInputMsg,
+		&subscription_callback, ON_NEW_DATA));
+	RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg, &cmd_vel_callback, ON_NEW_DATA));
+
+	//msg.data = 0;
+
+	while(1){
+		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10));
+		usleep(10000);
+	}
+
+	// free resources
+	RCCHECK(rcl_publisher_fini(&led_state_publisher, &node));
+    RCCHECK(rcl_publisher_fini(&fwd_distance_publisher, &node));
+    RCCHECK(rcl_publisher_fini(&left_distance_publisher, &node));
+	RCCHECK(rcl_publisher_fini(&right_distance_publisher, &node));
+	RCCHECK(rcl_subscription_fini(&led_input_subscriber, &node));
+	RCCHECK(rcl_subscription_fini(&subscriber, &node));
+	RCCHECK(rcl_node_fini(&node));
+
+  	vTaskDelete(NULL);
+}
+
+/*/ Function forward declarations
+void setupPins();
+void setupRos();
+void cmd_vel_callback(const void *msgin);
+void timer_callback(rcl_timer_t *timer, int64_t last_call_time);
+float fmap(float val, float in_min, float in_max, float out_min, float out_max);*/
+
+// We don't really need the callback, because msg is set anyway
+void cmd_vel_callback(const void *msgin) {
+//    const geometry_msgs__msg__Twist *msg = (const geometry_msgs__msg__Twist *) msgin;
+    printf("Message received.......................................: %f %f\n", msg.linear.x, msg.angular.z);
+}
+
+// Each frame, check msg data and set PWM channels accordingly
+
+
+
+void app_main(void)
+{   
+hcsr_setup_pins();
+
+#ifdef UCLIENT_PROFILE_UDP
+    // Start the networking if required
+    ESP_ERROR_CHECK(uros_network_interface_initialize());
+#endif  // UCLIENT_PROFILE_UDP
+
+    //pin micro-ros task in APP_CPU to make PRO_CPU to deal with wifi:
+    xTaskCreate(micro_ros_task, 
+            "uros_task", 
+            CONFIG_MICRO_ROS_APP_STACK, 
+            NULL,
+            CONFIG_MICRO_ROS_APP_TASK_PRIO, 
+            NULL); 
+}
+
+
+
+
+
+
+
+
+
+
+
